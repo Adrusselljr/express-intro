3fa2862c96044fbb634e2d89edd8df40a20c490b
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2022-02-08 15:55:21 -0500

    created express-generator

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # express-intro
+
+step 1: new repo<br>
+step 2: name repo<br>
+step 3: click add read.me<br>
+step 4: click.gitignore<br>
+step 5: select node<br>
+step 6: create repo<br>
+step 7: copy SSH<br>
+step 8: in terminal git clone SSH<br>
+step 9: cd into folder<br>
+step 10: in terminal npx express-generator --view=ejs backend<br>
+step 11: git status, git add ., git commit -m "created express-generator", git push<br>
+(npm i => if you get : cannot find module "ejs")<br>
---
+++ backend/routes/users.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/hello', function(req,res) {
+  res.send('Hello World from users')
+})
+
+router.get('/name/:firstname',function(req,res){
+  console.log(req.params);  //  /users/name/Bob -> {firstname: "Bob"} -> req.firstname = "Bob"
+  res.send("Check your log")
+})
+
+router.get('/name/:firstname/:lastname/car/:make/:model',function(req,res){
+  console.log(req.params);  //  /users/name/Bob -> {firstname: "Bob"} -> req.firstname = "Bob"
+  let carArr = [
+    {
+      id:0,
+      category: "Car",
+      make: 'BMW',
+      model: 'M3'
+    }
+  ]
+
+  let nameArr = [
+    {
+      id: 0,
+      category: "Name",
+      firstname: "Jay",
+      lastname: "Leno"
+    }
+  ]
+  let nameObj = {
+    id: nameArr.length,
+    category: "Name",
+    firstname: req.params.firstname,
+    lastname: req.params.lastname
+  }
+  let carObj = {
+    id: carArr.length,
+    category: "Car",
+    make: req.params.make,
+    model: req.params.model
+  }
+  carArr.push(carObj)
+  nameArr.push(nameObj)
+
+
+  let finalArr = [nameArr, carArr]
+  let outputObj = {
+    count: 2,
+    entries: finalArr
+
+  } 
+  res.send(outputObj)
+})
+
+router.get('/username', function(req, res) {
+  let username = process.env.username
+  res.send(username)
+})
+
+module.exports = router;
